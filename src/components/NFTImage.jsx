399a307a9d4bb8d765c78d6a332b816464403610
David <davidkhk@gmail.com>
2022-02-17 14:44:30 -0300

    Change placeholder image

--- src/components/NFTImage.jsx
+++ src/components/NFTImage.jsx
@@ -38,62 +38,65 @@ const NFTImage = ({ tokenId, getCount, contract }) => {
     
     return (
       <Center py={12}>
-      <Box
-        role={'group'}
-        p={6}
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}>
         <Box
-          rounded={'lg'}
-          mt={-12}
-          pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `url(${isMinted ? imageURI : '../assets/placeholder.png'})`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
-          <Image
-            rounded={'lg'}
-            height={230}
-            width={282}
-            objectFit={'contain'}
-            src={isMinted ? imageURI : '../assets/placeholder.png'}
-          />
+          role='group'
+          p={6}
+          maxW='330px'
+          w='full'
+          bg={useColorModeValue('white', 'gray.800')}
+          boxShadow='2xl'
+          rounded='lg'
+          pos='relative'
+          zIndex={1}>
+          <Box
+            rounded='lg'
+            mt={-12}
+            pos='relative'
+            height='230px'
+            _after={{
+              transition: 'all .3s ease',
+              content: '""',
+              w: 'full',
+              h: 'full',
+              pos: 'absolute',
+              top: 5,
+              left: 0,
+              background: `url(${isMinted ? imageURI : '../assets/placeholder.jpg'})`,
+              filter: 'blur(15px)',
+              zIndex: -1,
+            }}
+            _groupHover={{
+              _after: {
+                filter: 'blur(20px)',
+              },
+            }}>
+            <Image
+              rounded='lg'
+              height={230}
+              width={282}
+              objectFit='cover'
+              src={isMinted ? imageURI : '../assets/placeholder.jpg'}
+            />
+          </Box>
+          <Stack pt={10} align='center'>
+            <Text color='gray.600'>
+              ETH 0.0005
+            </Text>
+            <Text color='gray.500' fontSize='sm' textTransform='uppercase'>
+              ID #{tokenId}
+            </Text>
+            {!isMinted ? (
+              <Button colorScheme='teal' size='sm' onClick={mintToken}>
+                Mint
+              </Button>
+            ) : (
+              <Button colorScheme='teal' size='sm' onClick={getURI}>
+                Taken! Show URI
+              </Button>
+            )}
+          </Stack>
         </Box>
-        <Stack pt={10} align={'center'}>
-          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-            ID #{tokenId}
-          </Text>
-          {!isMinted ? (
-            <Button colorScheme='teal' size='sm' onClick={mintToken}>
-              Mint
-            </Button>
-          ) : (
-            <Button colorScheme='teal' size='sm' onClick={getURI}>
-              Taken! Show URI
-            </Button>
-          )}
-        </Stack>
-      </Box>
-    </Center>
+      </Center>
     );
   }
 
